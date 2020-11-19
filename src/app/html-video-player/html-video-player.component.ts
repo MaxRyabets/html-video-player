270165eca1b7a1be31f95b33e0ff3207507f86a6
Максим Рябец <maksaxel2@gmail.com>
2020-11-19 16:34:21 +0200

    feat(html-video-player): add progress bar and click on him

--- src/app/html-video-player/html-video-player.component.ts
+++ src/app/html-video-player/html-video-player.component.ts
@@ -1,21 +1,19 @@
-import {Component, OnInit, ViewChild} from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-html-video-player',
   templateUrl: './html-video-player.component.html',
-  styleUrls: ['./html-video-player.component.scss']
+  styleUrls: ['./html-video-player.component.scss'],
 })
 export class HtmlVideoPlayerComponent implements OnInit {
-
   readonly src = '../../assets/videos/movie.mp4';
 
   @ViewChild('video') video;
+  @ViewChild('progressBar') progressBar;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 
   playPause(): void {
     if (this.video.nativeElement.paused) {
@@ -25,5 +23,16 @@ export class HtmlVideoPlayerComponent implements OnInit {
     }
 
     this.video.nativeElement.pause();
+  }
+
+  seek(event: MouseEvent): void {
+    const percent = event.offsetX / this.video.nativeElement.offsetWidth;
+
+    this.video.nativeElement.currentTime =
+      percent * this.video.nativeElement.duration;
+
+    const target = event.target as HTMLTextAreaElement;
+    target.value = String(Math.floor(percent * 100));
+    target.innerHTML = this.progressBar.nativeElement.value + '% played';
   }
 }
