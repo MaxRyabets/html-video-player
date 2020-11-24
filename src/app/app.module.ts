d5b54c22b26e63e7531ff8406013e24477805048
Максим Рябец <maksaxel2@gmail.com>
2020-11-24 14:56:56 +0200

    fix(html-video-player): fix visible popup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { HtmlVideoPlayerComponent } from './html-video-player/html-video-player.component';
+import {AppComponent} from './app.component';
+import {HtmlVideoPlayerComponent} from './html-video-player/html-video-player.component';
 import {ReactiveFormsModule} from '@angular/forms';
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-import {OverlayModule} from '@angular/cdk/overlay';
 
 @NgModule({
   declarations: [AppComponent, HtmlVideoPlayerComponent],
-  imports: [BrowserModule, ReactiveFormsModule, NoopAnimationsModule, OverlayModule],
+  imports: [BrowserModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
