ca4ddb2b7a7e9e33ff9e4656304ff8ec8d3f26ca
Максим Рябец <maksaxel2@gmail.com>
2020-11-27 11:08:21 +0200

    feat(vdeio-play-list): output video play list and aa style for him

--- src/app/html-video-player/video-play-list/video-play-list.component.ts
+++ src/app/html-video-player/video-play-list/video-play-list.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { PlayList } from '../shared/play-list';
+import { VideoPlayListService } from './video-play-list.service';
+import { takeUntil, tap } from 'rxjs/operators';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-video-play-list',
   templateUrl: './video-play-list.component.html',
-  styleUrls: ['./video-play-list.component.scss']
+  styleUrls: ['./video-play-list.component.scss'],
 })
-export class VideoPlayListComponent implements OnInit {
+export class VideoPlayListComponent implements OnInit, OnDestroy {
+  playList: PlayList[] = [];
 
-  constructor() { }
+  duration = '';
+
+  destroy$ = new Subject();
+
+  constructor(private videoPlayListService: VideoPlayListService) {}
 
   ngOnInit(): void {
+    this.getPlayList();
   }
 
+  private getPlayList(): void {
+    this.videoPlayListService
+      .getPlayList()
+      .pipe(
+        takeUntil(this.destroy$),
+        tap((playList: PlayList[]) => {
+          this.playList = playList;
+        })
+      )
+      .subscribe();
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
 }
