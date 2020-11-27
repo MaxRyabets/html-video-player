9f01374936ebc708b453e578864590a5f21d1232
Максим Рябец <maksaxel2@gmail.com>
2020-11-27 09:11:17 +0200

    feat(html-video-player): init video play list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import {NgModule} from '@angular/core';
 import {AppComponent} from './app.component';
 import {HtmlVideoPlayerComponent} from './html-video-player/html-video-player.component';
 import {ReactiveFormsModule} from '@angular/forms';
+import { VideoPlayListComponent } from './html-video-player/video-play-list/video-play-list/video-play-list.component';
 
 @NgModule({
-  declarations: [AppComponent, HtmlVideoPlayerComponent],
+  declarations: [AppComponent, HtmlVideoPlayerComponent, VideoPlayListComponent],
   imports: [BrowserModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/html-video-player/video-play-list/video-play-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-video-play-list',
+  templateUrl: './video-play-list.component.html',
+  styleUrls: ['./video-play-list.component.scss']
+})
+export class VideoPlayListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
